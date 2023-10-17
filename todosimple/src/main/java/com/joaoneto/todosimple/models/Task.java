18e9c85923a20a02fcffee80806ec26ad89ccf07
Jbfaneto <jbfaneto@gmail.com>
2023-10-17 17:32:19 -0300

    Refactored code with lombok

--- todosimple/src/main/java/com/joaoneto/todosimple/models/Task.java
+++ todosimple/src/main/java/com/joaoneto/todosimple/models/Task.java
@@ -4,25 +4,25 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
 @Table(name = "task")
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Task implements Serializable{
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id", unique = true)
-    @Getter
-    @Setter
     private Long id;
 
-    @Getter
-    @Setter
     @ManyToOne
     @JoinColumn(name="user_id", nullable=false, updatable = false)
     private User user;
@@ -31,29 +31,6 @@ public class Task implements Serializable{
     @NotNull
     @NotEmpty
     @Size(min = 4, max = 255)
-    @Getter
-    @Setter
     private String description;
 
-    public Task(){
-
-    }
-
-    public Task(Long id, User user, String description) {
-        this.id = id;
-        this.user = user;
-        this.description = description;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Task task)) return false;
-        return Objects.equals(id, task.id) && Objects.equals(user, task.user) && Objects.equals(description, task.description);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, user, description);
-    }
 }
