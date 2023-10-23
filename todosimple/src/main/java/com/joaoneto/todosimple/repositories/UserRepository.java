208cfa30f6e6120dca3d179076396c1c957f9b54
Jbfaneto <jbfaneto@gmail.com>
2023-10-23 11:33:45 -0300

    Created UserDatailServiceImp and JWTUtil

--- todosimple/src/main/java/com/joaoneto/todosimple/repositories/UserRepository.java
+++ todosimple/src/main/java/com/joaoneto/todosimple/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.joaoneto.todosimple.repositories;
 
 import com.joaoneto.todosimple.models.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Transactional(readOnly = true)
+    User findByUsername (String username);
 }
---
+++ todosimple/src/main/java/com/joaoneto/todosimple/services/UserDetailsServiceImp.java
@@ -0,0 +1,26 @@
+package com.joaoneto.todosimple.services;
+
+import com.joaoneto.todosimple.models.User;
+import com.joaoneto.todosimple.repositories.UserRepository;
+import com.joaoneto.todosimple.security.UserSpringSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username);
+        if (Objects.isNull(user))
+            throw new UsernameNotFoundException("User not found: " + username);
+        return new UserSpringSecurity(user.getId(), user.getUsername(), user.getPassword(), user.getProfiles());
+    }
+}
